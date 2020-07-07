9541c95346823baa834b9e2e15cd4550d64d1c26
Kalev Alpernas <kalev.alp@gmail.com>
2020-07-07 19:00:37 +0300

    Added an async function case to the test

--- test/hello-world/handler.js
+++ test/hello-world/handler.js
@@ -5,8 +5,13 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function asyncSleep(ms, message) {
+    await sleep(ms).then(() => console.log(message));
+}
+
 module.exports.hello = keeper.wrapInKeeper(async event => {
     await sleep(300).then(() => console.log("Finished doing my asynchronous thing, can exit now!"))
+    asyncSleep(300, "Finished the second asynchronous think, can really exit now!")
     return {
         statusCode: 200,
         body: JSON.stringify(
