de8976eebc1c487549f77e7de2976e72e9884d5c
Kalev Alpernas <kalev.alp@gmail.com>
2020-11-02 17:41:04 +0200

    wip join

--- index.js
+++ index.js
@@ -2,20 +2,33 @@ const ah = require('async_hooks');
 const util = require('util');
 
 const unresolvedPromises = new Set();
+const out = [];
+
+let resolveJoin = () => {};
+let rejectJoin = () => {};
 
 const promiseHook =
       ah.createHook({
           init : (id, type, triggerAsyncId, resource) => {
+              out.push(util.inspect({id, type, triggerAsyncId, resource}));
+
               if (type === 'PROMISE' && !resource.isChainedPromise) {
                   unresolvedPromises.add(id);
               }
           },
           promiseResolve : (id) => {
+              debugger;
               if (unresolvedPromises.has(id)) {
                   unresolvedPromises.delete(id);
               }
+              if (!checkUnresolvedPromises()) {
+                  debugger;
+                  resolveJoin();
+              }
           },
       });
+
+promiseHook.enable();
 
 function checkUnresolvedPromises() {
     return unresolvedPromises.size !== 0;
@@ -23,16 +36,27 @@ function checkUnresolvedPromises() {
 
 function wrapInKeeper(handler) {
     return async (event, process) => {
-        promiseHook.enable();
+
 
         const response = await handler(event, process);
-        promiseHook.disable();
+
+        let joinPromise;
 
         if (checkUnresolvedPromises()) {
-            console.log("Unresolved promises exist!");
+            console.log("Unresolved promises exist!: ", unresolvedPromises);
+
+            joinPromise = new Promise((resolve, reject) => {
+                debugger;
+                resolveJoin = resolve;
+                rejectJoin = reject;
+            });
+        } else {
+            joinPromise = Promise.resolve(true);
         }
 
-        return response;
+        console.log(out);
+
+        return joinPromise.then(() => {return response;});
     }
 }
 
