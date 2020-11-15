bc86e72b73d3cb27e1219a03aa76bb1c26344768
Kalev Alpernas <kalev.alp@gmail.com>
2020-11-15 19:33:04 +0200

    starting switch to vm2 sandbox

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const ah = require('async_hooks');
 const util = require('util');
+const {NodeVM,VMScript} = require("vm2");
 
-const unresolvedPromises = new Set();
+
+const unresolvedPromises = {};
 const out = [];
+const stacks = [];
 
 let resolveJoin = () => {};
 let rejectJoin = () => {};
@@ -11,15 +14,16 @@ const promiseHook =
       ah.createHook({
           init : (id, type, triggerAsyncId, resource) => {
               out.push(util.inspect({id, type, triggerAsyncId, resource}));
+              stacks.push((new Error()).stack);
 
               if (type === 'PROMISE' && !resource.isChainedPromise) {
-                  unresolvedPromises.add(id);
+                  unresolvedPromises[id] = (new Error()).stack;
               }
           },
           promiseResolve : (id) => {
               debugger;
-              if (unresolvedPromises.has(id)) {
-                  unresolvedPromises.delete(id);
+              if (unresolvedPromises[id]) {
+                  delete unresolvedPromises[id];
               }
               if (!checkUnresolvedPromises()) {
                   debugger;
@@ -55,9 +59,106 @@ function wrapInKeeper(handler) {
         }
 
         console.log(out);
+        console.log(stacks);
 
         return joinPromise.then(() => {return response;});
     }
 }
+
+function createKeeperHandler(originalLambdaFile,
+                             originalLambdaHandler,
+                             runLocally,
+                             updateContext) {
+
+    const originalLambdaPath    = `${runLocally?'':'/var/task/'}${originalLambdaFile}`;
+    const originalLambdaCode    = fs.readFileSync(originalLambdaFile, 'utf8');
+    const originalLambdaScript  = new VMScript(originalLambdaCode);
+
+    const promiseForest = {};
+
+    const promiseWrapper = function (...params) {
+        const result = new Promise(...params);
+
+        promiseForest[result] = {};
+
+        result.shadow_catch = result.catch;
+        result.shadow_finally = result.finally;
+        result.shadow_then = result.then;
+
+        result.catch = function (...params) {
+            const catchPromise = result.shadow_catch(...params);
+            // Handle a call that returns a promise
+
+            if (promiseForest[this]) {
+                delete promiseForest[this];
+            }
+            promiseForest[catchPromise] = {};
+
+            return catchPromise;
+        };
+        result.finally = function (...params) {
+            const finallyPromise = result.shadow_finally(...params);
+            // Handle a call that returns a promise
+
+            if (promiseForest[this]) {
+                delete promiseForest[this];
+            }
+            promiseForest[finallyPromise] = {};
+
+            return finallyPromise;
+        };
+        result.then = function (...params) {
+            const thenPromise = result.shadow_then(...params);
+            // Handle a call that returns a promise
+
+            if (promiseForest[this]) {
+                delete promiseForest[this];
+            }
+            promiseForest[thenPromise] = {};
+
+            return thenPromise;
+        };
+
+        return result;
+    }
+
+    promiseWrapper.all = (...params) => {
+
+    }
+    promiseWrapper.allSettled = (...params) => {}
+    promiseWrapper.any = (...params) => {}
+    promiseWrapper.race = (...params) => {}
+    promiseWrapper.reject = (...params) => {}
+    promiseWrapper.resolve = (...params) => {}
+
+
+    const sandbox = {
+        process: process,
+        JSON: {
+            parse: JSON.parse,
+            stringify: JSON.stringify,
+        }
+        Promise: promiseWrapper
+    }
+
+    let executionEnv = {
+        console: 'inherit',
+        sandbox,
+        require: {
+            context: 'sandbox',
+            external: true,
+            builtin: ['*'],
+        },
+    };
+
+    const vm = new NodeVM(executionEnv);
+
+    const vmExports = vm.run(originalLambdaScript, originalLambdaPath);
+
+}
+
+
+
+
 
 module.exports.wrapInKeeper = wrapInKeeper;
